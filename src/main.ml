a462d7cb9b0723150e5eb2ac230c75928671af2d
Christian Lindig <christian.lindig@citrix.com>
2020-09-02 14:27:14 +0100

    Add option to use existing private key
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- src/main.ml
+++ src/main.ml
@@ -18,6 +18,11 @@ let write_substring fd string offset length =
   if written = length then R.ok ()
   else R.error_msgf "Failed to write %d bytes" length
 
+let read_pem path =
+  let ic = open_in path in
+  defer (fun () -> close_in ic) @@ fun () ->
+  really_input_string ic (in_channel_length ic)
+
 (** [write_cert] writes a PEM file to [path]. It attempts to do that
   atomically by writing to a temporary file in the same directory first and
   renaming the file at the end *)
@@ -60,8 +65,16 @@ let sign days key pubkey issuer req alt_names =
       |> R.reword_error (fun _ -> Printf.sprintf "signing failed" |> R.msg)
   | _ -> R.error_msgf "public/private keys don't match (%s)" __LOC__
 
-let selfsign name alt_names length days certfile =
-  let rsa = Rsa.generate ~bits:length () in
+let selfsign rsa_key_file name alt_names length days certfile =
+  let rsa =
+    match rsa_key_file with
+    | Some path -> (
+        read_pem path |> Cstruct.of_string |> X509.Private_key.decode_pem
+        |> R.get_ok
+        |> function
+        | `RSA x -> x )
+    | None -> Rsa.generate ~bits:length ()
+  in
   let privkey = `RSA rsa in
   let pubkey = `RSA (Rsa.pub_of_priv rsa) in
   let issuer =
@@ -81,13 +94,14 @@ let hostnames () =
   |> List.filter_map (fun addrinfo ->
          match addrinfo.Unix.ai_canonname with "" -> None | name -> Some name)
 
-let host name alt_names gethostnames pemfile =
+let host name alt_names gethostnames pemfile _rsa_key =
   let expire_days = 3650 in
   let length = 2048 in
   let alt_names =
     alt_names @ match gethostnames with false -> [] | true -> hostnames ()
   in
-  selfsign name alt_names length expire_days pemfile |> R.failwith_error_msg
+  selfsign _rsa_key name alt_names length expire_days pemfile
+  |> R.failwith_error_msg
 
 module CLI = struct
   let help =
@@ -123,10 +137,16 @@ module CLI = struct
       value & opt_all string []
       & info [ "a"; "alt" ] ~docv:"ALT" ~doc:"Add alternative hostname")
 
+  let rsa_key =
+    C.Arg.(
+      value
+      & opt (some file) None
+      & info [ "rsa" ] ~docv:"RSA" ~doc:"Use this private RSA key")
+
   let certify =
     let doc = "Create a self-signed certificate for a host" in
     C.Term.
-      ( const host $ hostname $ alt_names $ gethostnames $ pemfile
+      ( const host $ hostname $ alt_names $ gethostnames $ pemfile $ rsa_key
       , info "certify" ~doc ~man:help )
 
   let main () = C.Term.(exit @@ eval certify)
